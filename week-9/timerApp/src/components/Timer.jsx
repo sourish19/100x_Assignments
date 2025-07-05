7724dd443d74e13b7dd1704d1d374f3d9a62feed
sourish19 <sourish19z@gmail.com>
2025-07-05 13:28:17 +0000

    Updated Timer

--- week-9/timerApp/src/components/Timer.jsx
+++ week-9/timerApp/src/components/Timer.jsx
@@ -1,28 +1,78 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import style from "./Timer.module.css";
-import { useState } from "react";
-
-const Hour = () => {};
-
-const Minute = () => {};
-
-const Second = () => {};
+import { formatTime, calculateTime } from "../utils/auxiliaryFunctions.js";
 
 const Timer = () => {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [editState, setEditState] = useState(true);
+
+  const [hours, setHours] = useState("00");
+  const [minutes, setMinutes] = useState("00");
+  const [seconds, setSeconds] = useState("00");
+
+  useEffect(() => {}, []);
+
+  const handleInput = (e) => {
+    if (e.target.id === "hours") setHours(() => e.target.value);
+    if (e.target.id === "minutes") setMinutes(() => e.target.value);
+    if (e.target.id === "seconds") setSeconds(() => e.target.value);
+  };
+
   return (
     <div className={style.timerApp}>
       <div className={style.timerDisplay}>
         <div className={style.timerCircle}>
           <div className={style.timerTime}>
-            <span className={`${(style.timeInput, style.timeUnit)}`}>12:</span>
-            <span className={`${(style.timeInput, style.timeUnit)}`}>12:</span>
-            <span className={`${(style.timeInput, style.timeUnit)}`}>12</span>
+            {editState ? (
+              <input
+                className={style.timeInput}
+                id="hours"
+                onChange={handleInput}
+                type="text"
+                maxLength="2"
+              />
+            ) : (
+              <div>{hours + ":"}</div>
+            )}
+            {editState ? (
+              <input
+                className={style.timeInput}
+                id="minutes"
+                onChange={handleInput}
+                type="text"
+                maxLength="2"
+              />
+            ) : (
+              <div>{minutes + ":"}</div>
+            )}
+            {editState ? (
+              <input
+                className={style.timeInput}
+                id="seconds"
+                onChange={handleInput}
+                type="text"
+                maxLength="2"
+              />
+            ) : (
+              <div>{seconds}</div>
+            )}
           </div>
         </div>
       </div>
       <div className={style.actionButtons}>
-        <button className={style.actionButton}>Pause</button>
-        <button className={style.actionButton}>Stop</button>
+        <button
+          onClick={() => {
+            console.log(hours, minutes, seconds);
+
+            setTime(calculateTime(hours, minutes, seconds));
+            console.log(time);
+          }}
+          className={style.actionButton}
+        >
+          {isRunning ? "Pause" : "Start"}
+        </button>
+        <button className={style.actionButton}>Reset</button>
       </div>
     </div>
   );
