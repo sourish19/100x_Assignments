1128ec373237282055c4591992f60b105e51753a
sourish19 <sourish19z@gmail.com>
2025-07-05 10:49:00 +0000

    Initialized Assignment

--- week-9/timerApp/src/components/Timer.jsx
+++ week-9/timerApp/src/components/Timer.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import style from "./Timer.module.css";
+import { useState } from "react";
+
+const Hour = () => {};
+
+const Minute = () => {};
+
+const Second = () => {};
 
 const Timer = () => {
   return (
-    <div>Timer</div>
-  )
-}
+    <div className={style.timerApp}>
+      <div className={style.timerDisplay}>
+        <div className={style.timerCircle}>
+          <div className={style.timerTime}>
+            <span className={`${(style.timeInput, style.timeUnit)}`}>12:</span>
+            <span className={`${(style.timeInput, style.timeUnit)}`}>12:</span>
+            <span className={`${(style.timeInput, style.timeUnit)}`}>12</span>
+          </div>
+        </div>
+      </div>
+      <div className={style.actionButtons}>
+        <button className={style.actionButton}>Pause</button>
+        <button className={style.actionButton}>Stop</button>
+      </div>
+    </div>
+  );
+};
 
-export default Timer
+export default Timer;
